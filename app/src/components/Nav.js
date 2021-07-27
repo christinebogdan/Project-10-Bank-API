04b566177705a4810dc383e3357ff4ddcbba058e
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-07-27 19:47:18 +0200

    refactoring nav

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -3,13 +3,53 @@ import "../styles/nav.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { actionCreators } from "../store/actions";
 
 function Nav(props) {
+  const dispatch = useDispatch();
   const state = useSelector((state) => state.authentication);
-  console.log(state);
+  const firstName = state.userInfo.firstName;
+
+  // why does this not work in LINK
+  const loggingOut = (e) => {
+    console.log("Hello");
+    dispatch(actionCreators.loggingOut);
+    console.log(state);
+  };
+
+  const isAuthenticated = (firstName) => {
+    return (
+      <>
+        <div>
+          <FontAwesomeIcon icon="user-circle" />
+          <Link to="/" className="main-nav-item" onClick={loggingOut}>
+            {state.userInfo.firstName}
+          </Link>
+        </div>
+        <div>
+          <FontAwesomeIcon icon="sign-out-alt" />
+          <Link to="/" className="main-nav-item">
+            Sign Out
+          </Link>
+        </div>
+      </>
+    );
+  };
+
+  const notAuthenticated = () => {
+    return (
+      <div>
+        <FontAwesomeIcon icon="user-circle" />
+        <Link to="/login" className="main-nav-item">
+          Sign In
+        </Link>
+      </div>
+    );
+  };
+
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <a className="main-nav-logo" href="./">
         <img
           className="main-nav-logo-image"
           src={logo}
@@ -18,27 +58,9 @@ function Nav(props) {
         <h1 className="sr-only">Argent Bank</h1>
       </a>
       <div>
-        {state.isAuthenticated ? (
-          <>
-            <div>
-              <FontAwesomeIcon icon="user-circle" />
-              <Link className="main-nav-item">{state.userInfo.firstName}</Link>
-            </div>
-            <div>
-              <FontAwesomeIcon icon="sign-out-alt" />
-              <Link to="/login" className="main-nav-item" href="./sign-in.html">
-                Sign Out
-              </Link>
-            </div>
-          </>
-        ) : (
-          <div>
-            <FontAwesomeIcon icon="user-circle" />
-            <Link to="/login" className="main-nav-item" href="./sign-in.html">
-              Sign In
-            </Link>
-          </div>
-        )}
+        {state.isAuthenticated
+          ? isAuthenticated(firstName)
+          : notAuthenticated()}
       </div>
     </nav>
   );
