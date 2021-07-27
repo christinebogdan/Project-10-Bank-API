80e4ba059f6a2c73d5e856004241e7f34e64b9ea
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-07-27 19:23:54 +0200

    nav renders dynamically based on isAuthenticated state

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -2,8 +2,11 @@ import logo from "../designs/img/argentBankLogo.png";
 import "../styles/nav.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 function Nav(props) {
+  const state = useSelector((state) => state.authentication);
+  console.log(state);
   return (
     <nav className="main-nav">
       <a className="main-nav-logo" href="./index.html">
@@ -15,10 +18,27 @@ function Nav(props) {
         <h1 className="sr-only">Argent Bank</h1>
       </a>
       <div>
-        <FontAwesomeIcon icon="user-circle" />
-        <Link to="/login" className="main-nav-item" href="./sign-in.html">
-          Sign In
-        </Link>
+        {state.isAuthenticated ? (
+          <>
+            <div>
+              <FontAwesomeIcon icon="user-circle" />
+              <Link className="main-nav-item">{state.userInfo.firstName}</Link>
+            </div>
+            <div>
+              <FontAwesomeIcon icon="sign-out-alt" />
+              <Link to="/login" className="main-nav-item" href="./sign-in.html">
+                Sign Out
+              </Link>
+            </div>
+          </>
+        ) : (
+          <div>
+            <FontAwesomeIcon icon="user-circle" />
+            <Link to="/login" className="main-nav-item" href="./sign-in.html">
+              Sign In
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
