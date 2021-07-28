bff4f146a836991fa76c085db120849cb4ba53c0
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-07-28 09:58:14 +0200

    added custom History, added actions loggingOut, editName, firstName and lastName

--- app/src/store/actions.js
+++ app/src/store/actions.js
@@ -1,25 +1,33 @@
-// import { combineReducers } from "redux";
 import { requests } from "../helper/fetchRequest";
+import { customHistory } from "../helper/history";
 
 export const actions = {
   FORM_USERNAME: "form/username",
   FORM_PASSWORD: "form/password",
   FORM_REMEMBERME: "form/rememberme",
+  FORM_SUBMIT: "form/submit",
   LOGIN_REQUEST: "login/request",
   LOGIN_SUCCESS: "login/success",
   LOGIN_ERROR: "login/error",
   USER_LOGOUT: "user/logout",
-  USER_CHANGENAME: "user/changename",
+  USER_EDITNAME: "user/editname",
+  USER_FIRSTNAME: "user/firstname",
+  USER_LASTNAME: "user/lastname",
 };
 
 export const actionCreators = {
   username,
   password,
   rememberme,
+  // submit,
   request,
-  login,
-  logout,
   auth,
+  login,
+  // logout,
+  loggingOut,
+  editName,
+  firstName,
+  lastName,
 };
 
 function username(value) {
@@ -43,6 +51,12 @@ function rememberme(value) {
   };
 }
 
+function submit() {
+  return {
+    type: actions.FORM_SUBMIT,
+  };
+}
+
 function request(value) {
   return {
     type: actions.LOGIN_REQUEST,
@@ -51,6 +65,7 @@ function request(value) {
 }
 
 function auth(data) {
+  // how can I access history here?
   return (dispatch, getState) => {
     dispatch(request(data.username));
     console.log(getState());
@@ -61,11 +76,14 @@ function auth(data) {
         console.log(response);
         dispatch(login(response));
         console.log(getState());
+        customHistory.push("/profile");
       },
       (error) => {
         console.log(error);
       }
     );
+    dispatch(submit());
+    console.log(getState());
   };
 }
 
@@ -78,6 +96,34 @@ function login(value) {
 
 function logout() {
   return {
-    type: actions.LOGIN_ERROR,
+    type: actions.USER_LOGOUT,
+  };
+}
+
+function loggingOut() {
+  return (dispatch, getState) => {
+    console.log(sessionStorage);
+    sessionStorage.removeItem("user");
+    dispatch(logout);
+  };
+}
+
+function editName() {
+  return {
+    type: actions.USER_EDITNAME,
+  };
+}
+
+function firstName(value) {
+  return {
+    type: actions.USER_FIRSTNAME,
+    value,
+  };
+}
+
+function lastName(value) {
+  return {
+    type: actions.USER_LASTNAME,
+    value,
   };
 }
